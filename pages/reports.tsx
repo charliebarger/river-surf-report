0467cd96555f563ce93ed0cc64dc18a8099fdf94
Charlie <charliebarger04@gmail.com>
2023-05-18 08:45:34 -0600

    Add ssr

--- pages/reports.tsx
+++ pages/reports.tsx
@@ -1,11 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import Header from "@/components/utility/Header";
 import PageHeader from "@/components/utility/PageHeader";
 import { SectionWrapper } from "@/components/utility/SectionWrapper";
-import styles from "../styles/Home.module.css";
-import { Fragment } from "react";
 import Link from "next/link";
 import QuickInfoCard from "@/components/utility/QuickInfoCard";
 
@@ -21,127 +16,6 @@ interface Sites {
   }[];
 }
 
-const sites: Sites[] = [
-  {
-    continent: "North America",
-    countries: [
-      {
-        name: "United States",
-        flag: "🇺🇸",
-        abbreviation: "us",
-        states: [
-          { name: "Alabama" },
-          { name: "Alaska" },
-          { name: "Arizona" },
-          { name: "Arkansas" },
-          { name: "California" },
-          { name: "Colorado" },
-          { name: "Connecticut" },
-          { name: "Delaware" },
-          { name: "Florida" },
-          { name: "Georgia" },
-          { name: "Hawaii" },
-          { name: "Idaho" },
-          { name: "Illinois" },
-          { name: "Indiana" },
-          { name: "Iowa" },
-          { name: "Kansas" },
-          { name: "Kentucky" },
-          { name: "Louisiana" },
-          { name: "Maine" },
-          { name: "Maryland" },
-          { name: "Massachusetts" },
-          { name: "Michigan" },
-          { name: "Minnesota" },
-          { name: "Mississippi" },
-          { name: "Missouri" },
-          { name: "Montana" },
-          { name: "Nebraska" },
-          { name: "Nevada" },
-          { name: "New Hampshire" },
-          { name: "New Jersey" },
-          { name: "New Mexico" },
-          { name: "New York" },
-          { name: "North Carolina" },
-          { name: "North Dakota" },
-          { name: "Ohio" },
-          { name: "Oklahoma" },
-          { name: "Oregon" },
-          { name: "Pennsylvania" },
-          { name: "Rhode Island" },
-          { name: "South Carolina" },
-          { name: "South Dakota" },
-          { name: "Tennessee" },
-          { name: "Texas" },
-          { name: "Utah" },
-          { name: "Vermont" },
-          { name: "Virginia" },
-          { name: "Washington" },
-          { name: "West Virginia" },
-          { name: "Wisconsin" },
-          { name: "Wyoming" },
-        ],
-      },
-      {
-        name: "Canada",
-        flag: "🇨🇦",
-        abbreviation: "ca",
-        states: [
-          { name: "Ontario" },
-          { name: "Quebec" },
-          { name: "British Columbia" },
-        ],
-      },
-    ],
-  },
-  {
-    continent: "Europe",
-    countries: [
-      {
-        name: "Germany",
-        flag: "🇩🇪",
-        abbreviation: "de",
-        states: [
-          { name: "Bavaria" },
-          { name: "North Rhine-Westphalia" },
-          { name: "Baden-Württemberg" },
-        ],
-      },
-      {
-        name: "France",
-        flag: "🇫🇷",
-        abbreviation: "fr",
-        states: [
-          { name: "Île-de-France" },
-          { name: "Provence-Alpes-Côte d'Azur" },
-          { name: "Auvergne-Rhône-Alpes" },
-        ],
-      },
-    ],
-  },
-  {
-    continent: "Asia",
-    countries: [
-      {
-        name: "China",
-        flag: "🇨🇳",
-        abbreviation: "cn",
-        states: [
-          { name: "Guangdong" },
-          { name: "Zhejiang" },
-          { name: "Sichuan" },
-        ],
-      },
-      {
-        name: "Japan",
-        flag: "🇯🇵",
-        abbreviation: "jp",
-        states: [{ name: "Tokyo" }, { name: "Osaka" }, { name: "Hokkaido" }],
-      },
-    ],
-  },
-];
-
 interface TopFlows {
   waveName: string;
   cfs: number;
@@ -149,67 +23,39 @@ interface TopFlows {
   urlPraram: string;
 }
 
-const topFlows: TopFlows[] = [
-  {
-    waveName: "Munich City Wave",
-    cfs: 100,
-    countryFlag: "🇩🇪",
-    urlPraram: "munich-city-wave",
-  },
-  {
-    waveName: "Bend Whitewater Park",
-    cfs: 150,
-    countryFlag: "🇺🇸",
-    urlPraram: "bend-whitewater-park",
-  },
-  {
-    waveName: "Habitat 67 Wave",
-    cfs: 200,
-    countryFlag: "🇨🇦",
-    urlPraram: "habitat-67-wave",
-  },
-  {
-    waveName: "River Arno Wave",
-    cfs: 120,
-    countryFlag: "🇮🇹",
-    urlPraram: "river-arno-wave",
-  },
-  {
-    waveName: "Sevilla Wave",
-    cfs: 180,
-    countryFlag: "🇪🇸",
-    urlPraram: "sevilla-wave",
-  },
-];
-
-export default function Home() {
+export default function Reports({
+  sites,
+  topFlows,
+}: {
+  sites: Sites[];
+  topFlows: TopFlows[];
+}) {
   return (
     <>
       <Head>
         <title>Surf Otter : River Surf Reports</title>
         <meta name="description" content="The heart of River Surfing!" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
         <PageHeader>Reports</PageHeader>
         <SectionWrapper>
-          <div className=" lg:grid grid-cols-16 grid-flow-col gap-x-8 ">
+          <div className=" grid-flow-col grid-cols-16 gap-x-8 lg:grid ">
             {sites.map((region) => (
               <div key={region.continent} className=" col-start-1 ">
-                <h2 className=" text-lg font-bold  border-b-2 border-slate-200 pb-1 mt-4 mb-2 ">
+                <h2 className=" mt-4 mb-2  border-b-2 border-slate-200 pb-1 text-lg font-bold ">
                   {region.continent}
                 </h2>
                 {region.countries.map((country) => (
                   <div key={country.abbreviation} className="">
-                    <h3 className=" text-base font-bold mb-2 mt-4 text-slate-400 ">
+                    <h3 className=" mb-2 mt-4 text-base font-bold text-slate-400 ">
                       {country.name}
                     </h3>
                     <ul className="grid grid-cols-2 gap-2 md:grid-cols-3">
                       {country.states.map((state) => (
                         <li
                           key={state.name}
-                          className="text-linkColor hover:underline overflow-hidden whitespace-nowrap overflow-ellipsis"
+                          className="overflow-hidden overflow-ellipsis whitespace-nowrap text-linkColor hover:underline"
                         >
                           <Link
                             href={`/reports/${country.abbreviation}/${state.name}`}
@@ -223,16 +69,16 @@ export default function Home() {
                 ))}
               </div>
             ))}
-            <div className=" col-start-2">
+            <aside className=" col-start-2">
               <div>
-                <h2 className=" font-bold pb-1 mt-4 mb-2 text-xl ">
+                <h2 className=" mt-4 mb-2 pb-1 text-xl font-bold ">
                   Favorites
                 </h2>
                 <Link
                   href={"/sign-up"}
-                  className=" my-2 bg-white  px-4 py-2 rounded text-base border-2 border-[#0CAADC]
-        text-[#0CAADC] font-medium flex items-center
-        hover:bg-[#0CAADC] hover:text-white hover:border-white
+                  className=" my-2 flex  items-center rounded border-2 border-[#0CAADC] bg-white px-4
+        py-2 text-base font-medium text-[#0CAADC]
+        hover:border-white hover:bg-[#0CAADC] hover:text-white
         "
                 >
                   <span className=" ml-2 font-bold ">
@@ -241,7 +87,7 @@ export default function Home() {
                   <span className="ml-auto text-xl font-extrabold">+</span>
                 </Link>
               </div>
-              <h2 className=" font-bold pb-1 mt-4 mb-2 text-xl ">Top Flows</h2>
+              <h2 className=" mt-4 mb-2 pb-1 text-xl font-bold ">Top Flows</h2>
               <div>
                 {topFlows.map(({ waveName, cfs, countryFlag, urlPraram }) => (
                   <QuickInfoCard
@@ -253,10 +99,173 @@ export default function Home() {
                   />
                 ))}
               </div>
-            </div>
+            </aside>
           </div>
         </SectionWrapper>
       </main>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const sites: Sites[] = [
+    {
+      continent: "North America",
+      countries: [
+        {
+          name: "United States",
+          flag: "🇺🇸",
+          abbreviation: "us",
+          states: [
+            { name: "Alabama" },
+            { name: "Alaska" },
+            { name: "Arizona" },
+            { name: "Arkansas" },
+            { name: "California" },
+            { name: "Colorado" },
+            { name: "Connecticut" },
+            { name: "Delaware" },
+            { name: "Florida" },
+            { name: "Georgia" },
+            { name: "Hawaii" },
+            { name: "Idaho" },
+            { name: "Illinois" },
+            { name: "Indiana" },
+            { name: "Iowa" },
+            { name: "Kansas" },
+            { name: "Kentucky" },
+            { name: "Louisiana" },
+            { name: "Maine" },
+            { name: "Maryland" },
+            { name: "Massachusetts" },
+            { name: "Michigan" },
+            { name: "Minnesota" },
+            { name: "Mississippi" },
+            { name: "Missouri" },
+            { name: "Montana" },
+            { name: "Nebraska" },
+            { name: "Nevada" },
+            { name: "New Hampshire" },
+            { name: "New Jersey" },
+            { name: "New Mexico" },
+            { name: "New York" },
+            { name: "North Carolina" },
+            { name: "North Dakota" },
+            { name: "Ohio" },
+            { name: "Oklahoma" },
+            { name: "Oregon" },
+            { name: "Pennsylvania" },
+            { name: "Rhode Island" },
+            { name: "South Carolina" },
+            { name: "South Dakota" },
+            { name: "Tennessee" },
+            { name: "Texas" },
+            { name: "Utah" },
+            { name: "Vermont" },
+            { name: "Virginia" },
+            { name: "Washington" },
+            { name: "West Virginia" },
+            { name: "Wisconsin" },
+            { name: "Wyoming" },
+          ],
+        },
+        {
+          name: "Canada",
+          flag: "🇨🇦",
+          abbreviation: "ca",
+          states: [
+            { name: "Ontario" },
+            { name: "Quebec" },
+            { name: "British Columbia" },
+          ],
+        },
+      ],
+    },
+    {
+      continent: "Europe",
+      countries: [
+        {
+          name: "Germany",
+          flag: "🇩🇪",
+          abbreviation: "de",
+          states: [
+            { name: "Bavaria" },
+            { name: "North Rhine-Westphalia" },
+            { name: "Baden-Württemberg" },
+          ],
+        },
+        {
+          name: "France",
+          flag: "🇫🇷",
+          abbreviation: "fr",
+          states: [
+            { name: "Île-de-France" },
+            { name: "Provence-Alpes-Côte d'Azur" },
+            { name: "Auvergne-Rhône-Alpes" },
+          ],
+        },
+      ],
+    },
+    {
+      continent: "Asia",
+      countries: [
+        {
+          name: "China",
+          flag: "🇨🇳",
+          abbreviation: "cn",
+          states: [
+            { name: "Guangdong" },
+            { name: "Zhejiang" },
+            { name: "Sichuan" },
+          ],
+        },
+        {
+          name: "Japan",
+          flag: "🇯🇵",
+          abbreviation: "jp",
+          states: [{ name: "Tokyo" }, { name: "Osaka" }, { name: "Hokkaido" }],
+        },
+      ],
+    },
+  ];
+
+  const topFlows: TopFlows[] = [
+    {
+      waveName: "Munich City Wave",
+      cfs: 100,
+      countryFlag: "🇩🇪",
+      urlPraram: "munich-city-wave",
+    },
+    {
+      waveName: "Bend Whitewater Park",
+      cfs: 150,
+      countryFlag: "🇺🇸",
+      urlPraram: "bend-whitewater-park",
+    },
+    {
+      waveName: "Habitat 67 Wave",
+      cfs: 200,
+      countryFlag: "🇨🇦",
+      urlPraram: "habitat-67-wave",
+    },
+    {
+      waveName: "River Arno Wave",
+      cfs: 120,
+      countryFlag: "🇮🇹",
+      urlPraram: "river-arno-wave",
+    },
+    {
+      waveName: "Sevilla Wave",
+      cfs: 180,
+      countryFlag: "🇪🇸",
+      urlPraram: "sevilla-wave",
+    },
+  ];
+
+  return {
+    props: {
+      sites,
+      topFlows,
+    },
+  };
 }
