1881254031d33482e322cefcaca291f5502b480d
Charlie <charliebarger04@gmail.com>
2023-06-10 09:23:07 -0600

    style form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -22,42 +22,56 @@ const Contact = () => {
   } = useForm<ContactForm>();
   const wave = watch("waveName");
   return (
-    <main className="m-auto max-w-screen-xl px-5">
-      <form onSubmit={handleSubmit((data) => console.log(data))}>
+    <main className="m-auto my-8 max-w-screen-md px-5">
+      <form
+        className="grid grid-cols-3 gap-2 p-4"
+        onSubmit={handleSubmit((data) => console.log(data))}
+      >
+        <label className="flex ">
+          <span className="flex-1">Wave Name</span>
+          <input
+            className="flex-1 py-1 px-2"
+            {...register("waveName", { required: "This is requires" })}
+          />
+        </label>
         <input
-          {...register("waveName", { required: "This is requires" })}
-          placeholder="Wave Name"
-        />
-        <p>{wave}</p>
-        <p>{errors.waveName?.message}</p>
-        <input
+          className="py-1 px-2"
           {...register("parkingAddress", { required: "This is requires" })}
           placeholder="Parking Address"
         />
         <input
+          className="py-1 px-2"
           {...register("state", { required: "This is requires" })}
           placeholder="Province/State"
         />
         <input
+          className="py-1 px-2"
           {...register("country", { required: "This is requires" })}
           placeholder="Country"
         />
         <input
+          className="py-1 px-2"
           {...register("goodCondition")}
           placeholder="Good"
           type="number"
         />
         <input
+          className="py-1 px-2"
           {...register("fairCondition")}
           placeholder="Fair"
           type="number"
         />
         <input
+          className="py-1 px-2"
           {...register("flowSite")}
           placeholder="Site you Check your Flows at"
         />
-        <textarea {...register("other")} placeholder="Other" />
-        <input type="submit" value="Submit" />
+        <textarea
+          className="py-1 px-2"
+          {...register("other")}
+          placeholder="Other"
+        />
+        <input className="py-1 px-2" type="submit" value="Submit" />
       </form>
     </main>
   );
