c5fcb47473e6d33daeacee81e28a93562274bec5
Charlie <charliebarger04@gmail.com>
2023-06-13 08:52:49 -0600

    push site

--- components/utility/Footer.tsx
+++ components/utility/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className=' bg-primaryColor p-4 text-center'>
-      <p className=' text-white'>©2023 Swell God</p>
+    <footer className=" bg-primaryColor p-4 text-center">
+      <p className=" text-white">©2023 Surf Otter</p>
     </footer>
   );
 };
