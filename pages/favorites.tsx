ffbdb788ba3a144c167a14848f509e769580e69d
Charlie <charliebarger04@gmail.com>
2023-07-15 09:00:11 -0600

    add editing and sorting ui

--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -1,12 +1,12 @@
-import React, { PropsWithChildren, useState } from 'react';
-import { DetailedConditionsCardProps } from '@/components/utility/DetailedConditionsCard';
-import { RiverAPIReturn } from '@/helpers/API_Calls/riverData';
-import DetailedConditionsCard from '@/components/utility/DetailedConditionsCard';
-import { useSortable } from '@dnd-kit/sortable';
+import React, { PropsWithChildren, useState } from "react";
+import { DetailedConditionsCardProps } from "@/components/utility/DetailedConditionsCard";
+import { RiverAPIReturn } from "@/helpers/API_Calls/riverData";
+import DetailedConditionsCard from "@/components/utility/DetailedConditionsCard";
+import { useSortable } from "@dnd-kit/sortable";
 import {
   restrictToVerticalAxis,
   restrictToWindowEdges,
-} from '@dnd-kit/modifiers';
+} from "@dnd-kit/modifiers";
 import {
   DndContext,
   DragEndEvent,
@@ -20,96 +20,96 @@ import {
   DragOverlay,
   DragStartEvent,
   UniqueIdentifier,
-} from '@dnd-kit/core';
-import { useAutoAnimate } from '@formkit/auto-animate/react';
-import { CSS } from '@dnd-kit/utilities';
+} from "@dnd-kit/core";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { CSS } from "@dnd-kit/utilities";
 import {
   arrayMove,
   SortableContext,
   sortableKeyboardCoordinates,
   verticalListSortingStrategy,
-} from '@dnd-kit/sortable';
+} from "@dnd-kit/sortable";
 
 const mockRiverAxisData: RiverAPIReturn = {
   data: [
     {
-      dateTime: '2023-05-22T08:00:00',
+      dateTime: "2023-05-22T08:00:00",
       value: 10,
     },
     {
-      dateTime: '2023-05-22T09:00:00',
+      dateTime: "2023-05-22T09:00:00",
       value: 15,
     },
     {
-      dateTime: '2023-05-22T10:00:00',
+      dateTime: "2023-05-22T10:00:00",
       value: 20,
     },
     {
-      dateTime: '2023-05-22T11:00:00',
+      dateTime: "2023-05-22T11:00:00",
       value: 18,
     },
     {
-      dateTime: '2023-05-22T12:00:00',
+      dateTime: "2023-05-22T12:00:00",
       value: 25,
     },
     {
-      dateTime: '2023-05-22T13:00:00',
+      dateTime: "2023-05-22T13:00:00",
       value: 30,
     },
     {
-      dateTime: '2023-05-22T14:00:00',
+      dateTime: "2023-05-22T14:00:00",
       value: 28,
     },
     {
-      dateTime: '2023-05-22T15:00:00',
+      dateTime: "2023-05-22T15:00:00",
       value: 32,
     },
     {
-      dateTime: '2023-05-22T16:00:00',
+      dateTime: "2023-05-22T16:00:00",
       value: 35,
     },
     {
-      dateTime: '2023-05-22T17:00:00',
+      dateTime: "2023-05-22T17:00:00",
       value: 38,
     },
     {
-      dateTime: '2023-05-22T18:00:00',
+      dateTime: "2023-05-22T18:00:00",
       value: 36,
     },
     {
-      dateTime: '2023-05-22T19:00:00',
+      dateTime: "2023-05-22T19:00:00",
       value: 33,
     },
     {
-      dateTime: '2023-05-22T20:00:00',
+      dateTime: "2023-05-22T20:00:00",
       value: 28,
     },
     {
-      dateTime: '2023-05-22T21:00:00',
+      dateTime: "2023-05-22T21:00:00",
       value: 25,
     },
     {
-      dateTime: '2023-05-22T22:00:00',
+      dateTime: "2023-05-22T22:00:00",
       value: 23,
     },
     {
-      dateTime: '2023-05-22T23:00:00',
+      dateTime: "2023-05-22T23:00:00",
       value: 20,
     },
     {
-      dateTime: '2023-05-23T00:00:00',
+      dateTime: "2023-05-23T00:00:00",
       value: 18,
     },
     {
-      dateTime: '2023-05-23T01:00:00',
+      dateTime: "2023-05-23T01:00:00",
       value: 15,
     },
     {
-      dateTime: '2023-05-23T02:00:00',
+      dateTime: "2023-05-23T02:00:00",
       value: 13,
     },
     {
-      dateTime: '2023-05-23T03:00:00',
+      dateTime: "2023-05-23T03:00:00",
       value: 10,
     },
   ],
@@ -122,14 +122,14 @@ interface MockedRiverData extends DetailedConditionsCardProps {
 
 const mockWaveData: MockedRiverData[] = [
   {
-    id: '1',
+    id: "1",
     locationData: {
       wave: {
-        name: 'Boulder Creek Wave',
-        url: 'https://example.com/boulder-creek-wave',
+        name: "Boulder Creek Wave",
+        url: "https://example.com/boulder-creek-wave",
       },
-      state: 'Colorado',
-      country: 'United States',
+      state: "Colorado",
+      country: "United States",
     },
     riverData: {
       weather: {
@@ -147,14 +147,14 @@ const mockWaveData: MockedRiverData[] = [
     },
   },
   {
-    id: '2',
+    id: "2",
     locationData: {
       wave: {
-        name: 'Glenwood Wave',
-        url: 'https://example.com/glenwood-wave',
+        name: "Glenwood Wave",
+        url: "https://example.com/glenwood-wave",
       },
-      state: 'Colorado',
-      country: 'United States',
+      state: "Colorado",
+      country: "United States",
     },
     riverData: {
       weather: {
@@ -172,14 +172,14 @@ const mockWaveData: MockedRiverData[] = [
     },
   },
   {
-    id: '3',
+    id: "3",
     locationData: {
       wave: {
-        name: 'Animas River Wave',
-        url: 'https://example.com/animas-river-wave',
+        name: "Animas River Wave",
+        url: "https://example.com/animas-river-wave",
       },
-      state: 'Colorado',
-      country: 'United States',
+      state: "Colorado",
+      country: "United States",
     },
     riverData: {
       weather: {
@@ -197,14 +197,14 @@ const mockWaveData: MockedRiverData[] = [
     },
   },
   {
-    id: '4',
+    id: "4",
     locationData: {
       wave: {
-        name: 'Clear Creek Whitewater Park',
-        url: 'https://example.com/clear-creek-whitewater-park',
+        name: "Clear Creek Whitewater Park",
+        url: "https://example.com/clear-creek-whitewater-park",
       },
-      state: 'Colorado',
-      country: 'United States',
+      state: "Colorado",
+      country: "United States",
     },
     riverData: {
       weather: {
@@ -222,14 +222,14 @@ const mockWaveData: MockedRiverData[] = [
     },
   },
   {
-    id: '5',
+    id: "5",
     locationData: {
       wave: {
-        name: 'South Platte River Wave',
-        url: 'https://example.com/south-platte-river-wave',
+        name: "South Platte River Wave",
+        url: "https://example.com/south-platte-river-wave",
       },
-      state: 'Colorado',
-      country: 'United States',
+      state: "Colorado",
+      country: "United States",
     },
     riverData: {
       weather: {
@@ -304,11 +304,27 @@ const Favorites = () => {
   }
 
   return (
-    <main className='mb-8 px-4  md:px-6 lg:mb-10 '>
-      <h1 className='relative mx-auto my-6 max-w-4xl text-3xl font-semibold capitalize '>
-        Favorites
-      </h1>
-      <section className=' mx-auto max-w-4xl '>
+    <main className="mb-8 px-4  md:px-6 lg:mb-10 ">
+      <div className="mx-auto my-6 flex max-w-4xl items-center justify-between ">
+        <h1 className="relative text-3xl font-semibold capitalize ">
+          Favorites
+        </h1>
+        <button className="ml-2 self-center rounded-md border border-blue-500 py-1 px-2 text-sm font-semibold text-blue-500 ">
+          Edit
+        </button>
+      </div>
+      <div className="mx-auto my-6 flex max-w-4xl items-center gap-2  ">
+        <span>Sort By:</span>
+        <div className="flex-1 rounded border border-gray-400 bg-white px-2 ">
+          <select className="w-full py-2 ">
+            <option>My Order</option>
+            <option>Highest Flows</option>
+            <option>Rating</option>
+            <option>Alphabetical</option>
+          </select>
+        </div>
+      </div>
+      <section className=" mx-auto max-w-4xl ">
         <DndContext
           onDragEnd={handleDragEnd}
           onDragStart={handleDragStart}
@@ -320,7 +336,7 @@ const Favorites = () => {
             items={mockedCards.map((item) => item.id)}
             strategy={verticalListSortingStrategy}
           >
-            <ul className=' m-auto flex w-full max-w-4xl flex-col gap-6'>
+            <ul className=" m-auto flex w-full max-w-4xl flex-col gap-6">
               {mockedCards.map((item, index) => (
                 <DraggableWrapper key={item.id} id={item.id}>
                   <DetailedConditionsCard
